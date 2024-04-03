a78d4bb992263df790b56163bda8f7089b488eb8
ronnygranados <ronnygranados7@gmail.com>
2024-04-03 10:44:30 -0600

    Desactivación del WDT y correcto recibimiento del número pseudo-aleatorio

--- Laboratorio 1/dado.c
+++ Laboratorio 1/dado.c
@@ -1,5 +1,9 @@
 #include <pic14/pic12f683.h>
 
+// __CONFIG(_WDTE_OFF);
+typedef unsigned int word;
+word __at 0x2007 __CONFIG = (_WDT_OFF & _WDTE_OFF);
+
 // Se definen los pines en variables para facil manipulacion
 #define one 0b00000001
 #define two 0b00000010
@@ -13,23 +17,25 @@ void delay (unsigned int tiempo);
 unsigned int rand();
 
 // Variables globales
-unsigned int seed = 22032; // Necesito encontrar la manera de obtener un valor de seed diferente cada vez que se llame la función
+unsigned int seed = 22032;
 unsigned int led_matrix[] = {one, two, three, four, five, six};
-unsigned int rand_counter = 0; // Counter for updating seed
+unsigned int rand_counter = 0;
+
 
 // Funcion Main
 void main(void)
-{
+{   
     TRISIO = 0b00100000; //Poner todos los pines como salidas
 	GPIO = 0x00; //Poner pines en bajo
     ANSEL = 0x00; // Todas digitales
-    // unsigned int random;
+
     unsigned int time = 100;
 
     while (1)
     {   
         if (GP5 == 1)
-        {   
+        {
+            seed += rand_counter++;
             unsigned int random = rand();
             random = (random % 6);
 
@@ -56,7 +62,6 @@ unsigned int rand(){
     unsigned int q = 2348;
     unsigned int random_num;
 
-    seed += rand_counter++;
     random_num = (seed*seed) % p*q;
 
     return random_num;
